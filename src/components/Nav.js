bf141f85a6026158503014127856958d64f1d473
smckelli <smckelli@gmail.com>
2022-08-27 15:47:17 -0500

    completed rough Navbar and responsive transitions

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,24 @@
-import "./NavStyles.css"
+import "./NavStyles.css";
 
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Nav = () => {
+
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
   return (
     <div className="header">
         <Link to="/">
             <h1>Scott McKellips</h1>
         </Link>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : 
+        "nav-menu"}>
             <li>
                 <Link to="/">Home</Link>
             </li>
@@ -22,8 +31,15 @@ const Nav = () => {
             <li>
                 <Link to="/contact">Contact</Link>
             </li>
-
         </ul>
+        <div className="hamburger" onClick=
+            {handleClick}>
+            {click ? (
+            <FaTimes size={20} style={{ color: "#fff" }}/>
+            ) : (
+            <FaBars size={20} style={{ color: "#fff" }}/>
+            )};
+        </div>
     </div>
   )
 }
