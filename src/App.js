0fe1ef23b8d0cde1a1313de171218161682e18e9
smckelli <smckelli@gmail.com>
2022-08-27 10:22:03 -0500

    build basic scaffold for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import "./index.css"
+import "./index.css";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Project from "./pages/Project"
+import Contact from "./pages/Contact"
+
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div >
-
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/project" element={<Project />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/contact" element={<Contact />}/>
+    </Routes>
+    </>
   );
 }
 
