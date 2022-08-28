589771df90169a9aa8aee1414d4865a4ce6afa5b
smckelli <smckelli@gmail.com>
2022-08-28 12:43:21 -0500

    taking a break footer in progress

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import "./FooterStyles.css";
+
+import React from 'react'
+
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="footer-container">
+            <a href="https://github.com/smckelli" target="_blank">
+            <FaGithub 
+                size={30} 
+                style={{ color: "#fff", margineRight: "2rem" }} />
+            </a>
+            <a href="https://twitter.com" target="_blank">
+            <FaTwitter 
+                size={30} 
+                style={{ color: "#fff", margineRight: "2rem" }} />
+            </a>
+            <a href="https://linkedin.com/in/scott-mckellips" target="_blank">
+            <FaLinkedin 
+                size={30} 
+                style={{ color: "#fff", margineRight: "2rem" }} />
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
