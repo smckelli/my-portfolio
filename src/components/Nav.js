8e311c4e582cf307829f31f2e07d611432b3b042
smckelli <smckelli@gmail.com>
2022-08-28 11:32:25 -0500

    sticky nav in place

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,9 +10,20 @@ const Nav = () => {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
 
+    const [color, setColor] = useState(false);
+    const changeColor = () =>{
+        if (window.scrollY >= 1) {
+            setColor(true);
+        }else{
+            setColor(false);
+        }
+    };
+
+    window.addEventListener("scroll", changeColor);
+
 
   return (
-    <div className="header">
+    <div className={color ? "header header-bg" : "header"}>
         <Link to="/">
             <h1>Scott McKellips</h1>
         </Link>
