52daba7e6a07bf1bfa9fa50c10e04f338a16292e
smckelli <smckelli@gmail.com>
2022-08-30 08:29:06 -0500

    fixed small syntax error on imports

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
-import HeroPro from "../components/HeroPro";
-import WorkCard from "../components/WorkCard";
+import Nav from "../components/Nav.js";
+import Footer from "../components/Footer.js";
+import HeroPro from "../components/HeroPro.js";
+import WorkCard from "../components/WorkCard.js";
 
 const Project = () => {
   return (
