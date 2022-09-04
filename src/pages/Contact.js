bee26d18e2317f2903843dead3aa2e751c3387e6
smckelli <smckelli@gmail.com>
2022-09-04 11:35:23 -0500

    basic portfolio structure in place.... now to tweak

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Nav from "../components/Nav";
 import HeroCon from "../components/HeroCon";
 import Footer from "../components/Footer";
+import Form from "../components/Form";
 
 const Contact = () => {
   return (
     <div>
     <Nav />
     <HeroCon heading="Contact Me" text="I would love to hear from you!" />
+    <Form />
     <Footer />
     </div>
   )
