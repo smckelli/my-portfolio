6fc14ceed5f6bc21dc36d625bd48b61ea3242ee9
smckelli <smckelli@gmail.com>
2022-09-08 19:20:26 -0500

    filling out small things like resume and about

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -2,7 +2,7 @@ import "./AboutContentStyles.css";
 
 import React from 'react';
 import { Link } from "react-router-dom";
-import Avascott from "../assets/Avascott.png";
+// import Avascott from "../assets/Avascott.png";
 import familysunflowers from "../assets/familysunflowers.jpg";
 
 const AboutContent = () => {
@@ -10,7 +10,9 @@ const AboutContent = () => {
     <div className="about">
         <div classname="left">
             <h1>My Story</h1>
-            <p>This is my story.</p>
+            <p>Dad to two great kids and husband to my amazing and talented wife,</p>
+            <p>I am looking for an opportunity in Full Stack Web Development.</p>
+            <p></p>
             <Link to="/contact">
                 <button className="btn">Contact</button>
             </Link>
@@ -18,10 +20,10 @@ const AboutContent = () => {
 
         <div className="right">
             <div className="img-container">
-                <div className="img-stack top">
+                {/* <div className="img-stack top">
                     <img src={Avascott}
                     className="img" alt="scotts avatar"/>
-                </div>
+                </div> */}
                 <div className="img-stack bottom">
                     <img src={familysunflowers}
                     className="img" alt="family photo"/>
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from "../components/Nav";
+import ResumeContent from "../components/ResumeContent";
 import Footer from "../components/Footer";
 
 const Resume = () => {
   return (
     <div>
     <Nav />
+    <ResumeContent />
     <Footer />
     </div>
   )
