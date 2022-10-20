e44ab4cf35b352edc8ba97739dc5f514325c5b4a
smckelli <smckelli@gmail.com>
2022-10-20 14:11:41 -0500

    added the form functionality to the form on contact page

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,46 @@
 import "./FormStyles.css";
 
-import React from 'react'
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm(
+      'service_kr9s60b', 
+      'template_rcbsf31', 
+      form.current, 
+      '4nAYSSX0mk4KnOPtz')
+
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      }
+    );
+  }
+
+
   return (
     <div className="form">
       {/* The functionality of the form will be in a later build */}
-        <form>
-            {/* <label>Your Name</label>
-            <input type="text"></input>
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Your Name</label>
+            <input type="text" name= "user_name" size="40"></input>
             <label>Email</label>
-            <input type="email"></input>
+            <input type="email" name= "user_email" size ="40"></input>
             <label>Subject</label>
-            <input type="text"></input>
+            <input type="text" size="40"></input>
             <label>Message</label>
-            <textarea rows="6" placeholder="Type your message here" />
-            <button className="btn">Submit</button> */}
-            <h4>Basement Headquarters</h4>
+            <textarea rows="6" cols="40" placeholder="Type your message here" name="message" size="50"/>
+            <button className="btn" type="submit" value="Send">Submit</button>
+            {/* <h4>Basement Headquarters</h4>
             <p>Sun Prairie, Wisconsin</p>
             <p></p>
             <h4>Branch Offices</h4>
@@ -24,7 +48,7 @@ const Form = () => {
             <p>Wisla-Czarne, Poland</p>
             <p></p>
             <h4>Email</h4>
-            <a href="mailto:starypies22@gmail.com">starypies22@gmail.com</a>
+            <a href="mailto:starypies22@gmail.com">starypies22@gmail.com</a> */}
         </form>
 
     </div>
