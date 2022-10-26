3258538119d4df9aba579a8bf28039a3d3cdfd20
smckelli <smckelli@gmail.com>
2022-10-26 14:07:33 -0500

    added python and fiixed minor typo

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -4,6 +4,7 @@ import project3 from "../assets/project3.jpg";
 import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
 import project6 from "../assets/project6.png";
+import project7 from "../assets/project7.png";
 
 const ProjectCardData = [
     {
@@ -29,20 +30,27 @@ const ProjectCardData = [
     },
     {
         imgsrc: project4,
+        title: "Just Tech News- Python/ APIs",
+        text: "I use this Python application to serve as a repository for troubleshooting solutions found online.",
+        view: "https://py-just-tech-news.herokuapp.com/",
+        source: "https://github.com/smckelli/python-newsfeed"
+    },
+    {
+        imgsrc: project5,
         title: "Weather Dashboard- Server-side APIs",
-        text: "A weather dashboard to display the current weather and 5 day forecast for any city in the United STates and the world.",
+        text: "A weather dashboard to display the current weather and 5 day forecast for any city in the United States and the world.",
         view: "https://smckelli.github.io/weather-dashboard/",
         source: "https://github.com/smckelli/weather-dashboard"
     },
     {
-        imgsrc: project5,
+        imgsrc: project6,
         title: "Note Taker- Express.js",
         text: "A note-taking application to write and save notes as a way or organize your thoughts and keep track of your tasks.",
         view: "https://smckelli-note-taker.herokuapp.com/",
-        source: "https://github.com/smckelli/note-taker"
+        source: "https://github.com/smckelli/note-taker",
     },
     {
-        imgsrc: project6,
+        imgsrc: project7,
         title: "Work Day Scheduler- Third-party APIs",
         text: "This is a calendar application to track any event during each hour of a typical business day between the hours of 0900-1800.",
         view: "https://smckelli.github.io/scheduler/",
